00f83a8c8fdf0ac0dc30c01e3688f8ed5b95cb99
sachiniwathudura <sachiniwathudura@gmail.com>
2023-12-17 19:50:14 +0530

    refactor complete OrderDetailDAOImpl

---
+++ src/main/java/com/example/layeredarchitecture/dao/OrderDetailDAO.java
@@ -0,0 +1,10 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.model.OrderDetailDTO;
+
+import java.sql.SQLException;
+
+public interface OrderDetailDAO {
+     boolean saveOrderDetail(String orderId, OrderDetailDTO dto) throws SQLException, ClassNotFoundException ;
+
+}
