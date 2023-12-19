edd4d4c59f6ff6f52f59581972850f555e810e87
sachiniwathudura <sachiniwathudura@gmail.com>
2023-12-19 22:56:20 +0530

    CrudDAO

---
+++ src/main/java/com/example/layeredarchitecture/dao/CrudDAO.java
@@ -0,0 +1,24 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.model.CustomerDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDAO <T>{
+    ArrayList<T> getAll() throws SQLException, ClassNotFoundException ;
+
+    boolean save(T dto) throws SQLException, ClassNotFoundException ;
+
+    boolean update(T dto) throws SQLException, ClassNotFoundException ;
+
+    boolean exsit(String id) throws SQLException, ClassNotFoundException ;
+
+    void delete(String id) throws SQLException, ClassNotFoundException ;
+
+    String generateNewId() throws SQLException, ClassNotFoundException ;
+
+   // ArrayList <T> loadAllIds() throws SQLException,ClassNotFoundException;
+
+   T search(String id) throws SQLException, ClassNotFoundException;
+}
